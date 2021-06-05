bfe37a5aef796cec3606b0458cc940364f147146
mehdi fathi nezhad <82803510+mehdifn@users.noreply.github.com>
2021-06-05 11:15:53 +0430

    Create main ... .py

---
+++ main ... .py
@@ -0,0 +1,86 @@
+"این فایل در حال کامل شدن است "
+# coming soon . . !
+
+
+import json
+import moshtari
+import froshgah
+import froshande
+import فایل مشتری ها
+
+
+def main():
+    operator = {'username': 'godfather' , 'password': '12345678'}
+
+    print("به فروشگاه خوش آمدید")
+    print("ورود به عنوان:")
+    print("1.مشتری \n 2.فروشنده \n 3.اپراتور")
+
+    voroduser = int(input())
+    switch(voroduser){
+
+        case 1:#مشتری
+
+            vorood = int(input("1.ورود \n 2.ثبت نام"))
+            switch(vorood){
+
+                case 1:#ورود مشتری
+                    nam = str(input("نام کاربری خود را وارد کنید:")
+                    pas = str(input("رمز خود را وارد کنید:"))
+
+                    open("useraccount.txt")
+                        for i in useraccount:
+                            if (name == nam and pas == pass):
+
+                                #منوی مشتری
+                               .
+                               .
+                               .
+
+                            else:
+                                print("نام کاربری یا رمزعبور یافت نشد! ")
+                                break
+
+                case 2:#ثبت نام مشتری 
+                    nam = str(input("نام کاربری خود را وارد کنید:")
+                    pas = str(input("رمز خود را وارد کنید:"))
+
+                    open("useraccount.txt")
+                        for i in useraccount:
+                            if (name == nam and pas == pass):
+                                print("این کاربر قبلا ثبت نام کرده است از منوی اصلی وارد شوید")
+                                break
+                            
+                            else: 
+                                print("تبریک! کاربری با این نام کاربری ثبت نشده \n لطفا مابقی اطلاعات را کامل کنید:")
+                                id=cu+str(input("لطفا 6 رقم اول کد ملی خود را وارد کنید:"))
+                                adres = str(input("ادرس خود را وارد کنید:"))
+                                area = int(input(" نام منطقه خود را به صورت عدد وارد کنید:)"))
+                                phone = int(input("شماره تلغن همراه خود را وارد کنید:"))
+
+-----------                                فایل مشتری ها.dump(id = moshtari())
+                            
+                                
+
+
+
+
+            }
+
+                    
+                
+
+
+
+
+
+        case 2:#فروشنده
+            . . . 
+        
+        case 3:#اپراتور
+            . . .
+    
+    }
+
+    return True
+    
