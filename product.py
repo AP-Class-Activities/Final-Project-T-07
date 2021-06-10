3a1bb30f82bfe1b05eaabe29efc115b6fbc0255e
Parsay2k <82779818+Parsay2k@users.noreply.github.com>
2021-06-10 13:16:58 +0430

    Add files via upload

---
+++ product.py
@@ -0,0 +1,74 @@
+'''
+This is a class defining a product in online shop.
+You should notice that this class simpifies the product type.
+Someone may add more features and bahavors to this class.
+
+
+Usage:
+   1) Create a new product:
+        p= product(name,price,number)
+
+   2) Print the product information:    
+        print(p)
+
+    
+'''
+
+from random import seed
+from random import randint
+
+class Product():
+    def __init__(self , name_of_product , price , num ):
+        self.__name_of_product = name_of_product
+        self.__price_of_product = price
+        if int(num) <= 0:
+            raise ValueError('the number of product should be a Natural number!')
+        self.__number_of_product = int(num)
+        seed(1)
+        for _ in range(1):
+            value = randint(100000 , 999999)
+            self.__product_id = "PR" + str(value)
+        
+    #setters and getters
+    @property
+    def name_of_product(self):
+        return self.__name_of_product
+
+    @name_of_product.setter
+    def name_of_product(self,value):
+        self.__name_of_product = value
+
+    @property
+    def price_of_product(self):
+        return self.__price_of_product
+
+    @price_of_product.setter
+    def price_of_product(self,value):
+        self.__price_of_product = value
+
+    @property
+    def number_of_product(self):
+        return self.__number_of_product
+
+    @number_of_product.setter
+    def number_of_product(self,value):
+        if value <= 0:
+            raise ValueError('the number of product should be a Natural number!')
+        self.__number_of_product = value
+
+    @property
+    def product_id(self):
+        return self.__product_id
+
+    @product_id.setter
+    def product_id(self,value):
+        self.__product_id = value
+
+    def __str__(self):
+        return 'name:{}   ID:{}   price:{}   number:{}'\
+            .format(self.__name_of_product,self.__product_id,self.__price_of_product,self.__number_of_product)
+
+'''
+p=Product('story_book','42000','10')
+print(p)
+'''
